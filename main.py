d6ad0011aa4be732dff0114a51cdd8c4e6e46748
Marian Smaranda <marian.smaranda97@gmail.com>
2021-05-30 13:16:45 +0300

    Adaug modificarile facute programului

--- main.py
+++ main.py
@@ -2,14 +2,18 @@
 from auth import Auth
 
 while True:
-    meniu = ["Login","Shut down!"]
-    for submeniu in range(len(meniu)):
-        print(f'{submeniu + 1}.{meniu[submeniu]}')
-    optiune = int(input('> ')) - 1
-    if meniu[optiune] == 'Shut down!':
+    meniu = ["1.Login","2.Shut down!"]
+    for submeniu in meniu:
+        print(submeniu)
+    optiune = input('> ')
+    if optiune == '2':
         print('Shutting Down...')
         break
-    elif optiune == 0:
+    elif optiune == "1":
         client = Auth()
         user = client.login()
+    else:
+        print()
+        print("Alege una din optiunile afisate!")
+        print()
 
--- menus.py
+++ menus.py
@@ -6,25 +6,25 @@ from productie import Productie
 class Menu:
     def meniu_manager(self):
         while True:
-            meniu = ["Angajeaza Manager", 'Angajeaza Operator', 'Angajeaza Gestionar', 'Afiseaza Angajatii',
-                     "Log Out"]
+            meniu = ["1.Angajeaza Manager", '2.Angajeaza Operator', '3.Angajeaza Gestionar', '4.Afiseaza Angajatii',
+                     "5.Log Out"]
 
-            for submeniu in range(len(meniu)):
-                print(f'{submeniu + 1}.{meniu[submeniu]}')
-            optiune = int(input('> ')) - 1
-            if meniu[optiune] == 'Log Out':
+            for submeniu in meniu:
+                print(submeniu)
+            optiune = input('> ')
+            if optiune == '5':
                 print('Logging Out...')
                 break
-            elif optiune == 0:
+            elif optiune == "1":
                 Angajator.angajator_manager(self)
                 pass
-            elif optiune == 1:
+            elif optiune == "2":
                 Angajator.angajator_operator(self)
                 pass
-            elif optiune == 2:
+            elif optiune == "3":
                 Angajator.angajator_gestionar(self)
                 pass
-            elif optiune == 3:
+            elif optiune == "4":
                 with open("users.json", "r") as u:
                     angajati = json.load(u)
                     print("Manageri")
@@ -49,43 +49,57 @@ class Menu:
                     print(20 * "=")
                     print()
                 pass
+            else:
+                print()
+                print("Alege una din optiunile afisate!")
+                print()
+
     def meniu_operator(self):
         while True:
-            meniu = ['Asambleaza Frigider', 'Asambleaza Microunde', 'Asambleaza aragaz',
-                     'Log Out']
-            for submeniu in range(len(meniu)):
-                print(f'{submeniu + 1}.{meniu[submeniu]}')
-            optiune = int(input('> ')) - 1
-            if meniu[optiune] == 'Log Out':
+            meniu = ['1.Asambleaza Frigider', '2.Asambleaza Microunde', '3.Asambleaza aragaz',
+                     '4.Log Out']
+            for submeniu in meniu:
+                print(submeniu)
+            optiune = input('> ')
+            if optiune == '4':
                 print('Logging Out...')
                 break
-            elif optiune == 0:
+            elif optiune == "1":
                 Productie.productie_frigidere(self)
-            elif optiune == 1:
+            elif optiune == "2":
                 Productie.productie_cuptor_microunde(self)
-            elif optiune == 2:
+            elif optiune == "3":
                 Productie.productie_aragaz(self)
+            else:
+                print()
+                print("Alege una din optiunile afisate!")
+                print()
+
     def meniu_gestionar(self):
         while True:
-            meniu = ['Verifica Stock', 'Adauga Tevi','Adauga Placi Metal','Adauga Placi Electronice',
-                     'Adauga Foi Metal' ,'Adauga Plastic' , 'Log Out']
-            for submeniu in range(len(meniu)):
-                print(f'{submeniu + 1}.{meniu[submeniu]}')
-            optiune = int(input('> ')) - 1
-            if meniu[optiune] == 'Log Out':
+            meniu = ['1.Verifica Stock', '2.Adauga Tevi','3.Adauga Placi Metal','4.Adauga Placi Electronice',
+                     '5.Adauga Foi Metal' ,'6.Adauga Plastic' , '7.Log Out']
+            for submeniu in meniu:
+                print(submeniu)
+            optiune = input('> ')
+            if optiune == '7':
                 print('Logging Out...')
                 break
-            elif optiune == 0:
+            elif optiune == "1":
                 with open("stocuri.json", "r") as s:
                     stoc = json.load(s)
                     print(stoc)
-            elif optiune == 1:
+            elif optiune == "2":
                 Stocuri.adauga_stoc_tevi(self)
-            elif optiune == 2:
+            elif optiune == "3":
                 Stocuri.adauga_stoc_placi_metal(self)
-            elif optiune == 3:
+            elif optiune == "4":
                 Stocuri.adauga_stoc_placi_electronice(self)
-            elif optiune == 4:
+            elif optiune == "5":
                 Stocuri.adauga_stoc_foi_metal(self)
-            elif optiune == 5:
+            elif optiune == "6":
                 Stocuri.adauga_stoc_plastic(self)
+            else:
+                print()
+                print("Alege una din optiunile afisate!")
+                print()
